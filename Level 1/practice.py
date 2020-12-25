a400a617435a65ece804a2a41ee8f2195df22d6b
tolom@naver.com <Skir131>
2020-12-26 06:45:52 +0900

    pracitce

--- Level 1/practice.py
+++ Level 1/practice.py
@@ -1,6 +1,10 @@
-num = int(input("세 자리 정수를 입력하세요 : "))
-print(num % 10)
-num = num // 10
-print(num % 10)
-num = num // 10
-print(num % 10)
+def sort(a):
+    if not a:
+        return []
+    pivot = a[0]
+    return sort([i for i in a[1:] if i+pivot > pivot+i]) + [pivot] + sort([i for i in a[1:] if i+pivot <= pivot + i])
+    
+_ = input()
+numbers = list(map(str, input().split()))
+result = sort(numbers)
+print(str(int("".join((numbers)))))
