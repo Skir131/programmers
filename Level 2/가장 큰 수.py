d1fc0b90b3f1cfa72d9536c043b702583ce2e381
tolom@naver.com <Skir131>
2020-12-26 18:48:48 +0900

    12/26 해결

--- Level 2/가장 큰 수.py
+++ Level 2/가장 큰 수.py
@@ -1,7 +1,9 @@
 def sort(a):
+    # 퀵소트 방식으로 문제 해결
     if not a:
         return []
     pivot = a[0]
+    # 비교 조건 예 : 6, 10에 대하여 610, 106을 비교하고 610이 앞으로 오도록 한다. 
     return sort([i for i in a[1:] if i+pivot > pivot+i]) + [pivot] + sort([i for i in a[1:] if i+pivot <= pivot + i])
     
                     
